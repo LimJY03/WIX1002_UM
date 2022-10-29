479ded55f1c1e12fc0977dea398527fdda7c93e3
DesigneR- <limjunyi03@gmail.com>
2022-10-29 14:32:15 +0800

    Create L1Q3.java

---
+++ Lab 01/L1Q3.java
@@ -0,0 +1,15 @@
+public class L1Q3 {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("*********      ***        *          *      ");
+        System.out.println("*       *     *   *      ***        * *     ");
+        System.out.println("*       *    *     *    *****      *   *    ");
+        System.out.println("*       *    *     *      *       *     *   ");
+        System.out.println("*       *    *     *      *      *       *  ");
+        System.out.println("*       *    *     *      *       *     *   ");
+        System.out.println("*       *    *     *      *        *   *    ");
+        System.out.println("*       *     *   *       *         * *     ");
+        System.out.println("*********      ***        *          *      ");
+    }
+}
