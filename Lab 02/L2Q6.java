00da66fba26e11eea5b365523c8eb846e5e7798b
DesigneR- <limjunyi03@gmail.com>
2022-10-31 11:57:49 +0800

    Create L2Q6.java

---
+++ Lab 02/L2Q6.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class L2Q6 {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        L2Q1 converter = new L2Q1();
+        
+        double M = 0.0, Q = 0.0, Tf = 0.0, Ti = 0.0;
+
+        System.out.print("Enter the amount of water in gram: ");
+        M = sc.nextDouble();
+
+        System.out.print("Enter the initial temperature in Fahrenheit: ");
+        Ti = converter.fahrCels(sc.nextDouble());
+
+        System.out.print("Enter the final temperature in Fahrenheit: ");
+        Tf = converter.fahrCels(sc.nextDouble());
+
+        Q = M * (Tf - Ti) * 4184;
+
+        System.out.printf("The energy needed is %.2f joules.", Q);
+
+        sc.close();
+    }
+}
