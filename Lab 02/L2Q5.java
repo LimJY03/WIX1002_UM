b09903ca15f3401036c3f6765cc93efc784db07b
DesigneR- <limjunyi03@gmail.com>
2022-10-31 11:57:44 +0800

    Create L2Q5.java

---
+++ Lab 02/L2Q5.java
@@ -0,0 +1,17 @@
+public class L2Q5 {
+    
+    public static void main(String[] args) {
+        
+        int randNum = (int) Math.round(Math.random() * 10000), sum = 0;
+
+        // Recursion
+        int rec = randNum;
+
+        while (rec % 10 != 0) {
+            sum += rec % 10;
+            rec = rec / 10;
+        }
+
+        System.out.printf("The sum of all digits in %d is %d.", randNum, sum);
+    }
+}
