9a41780942ced28e3c40d60c23a48e48fa55cb77
DesigneR- <limjunyi03@gmail.com>
2022-10-31 11:58:16 +0800

    Added Convert Method

--- Lab 02/L2Q1.java
+++ Lab 02/L2Q1.java
@@ -5,13 +5,19 @@ public class L2Q1 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        L2Q1 converter = new L2Q1();
+        
         double cels = 0.0, fahr = 0.0;
 
         fahr = sc.nextDouble();
-        cels = (fahr - 32) / 1.8;
+        cels = converter.fahrCels(fahr);
 
         System.out.printf("%f Fahrenheit is %.2f Celsius.\n", fahr, cels);
 
         sc.close();
+    }
+
+    double fahrCels(double fahr) {
+        return (fahr - 32) / 1.8;
     }
 }
