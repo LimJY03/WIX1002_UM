7f4c0ae325b3b365ae7e327c7c5e26c5f0e2cbc8
DesigneR- <limjunyi03@gmail.com>
2022-10-31 11:27:25 +0800

    Create L2Q1.java

---
+++ Lab 02/L2Q1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class L2Q1 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        double cels = 0.0, fahr = 0.0;
+
+        fahr = sc.nextDouble();
+        cels = (fahr - 32) / 1.8;
+
+        System.out.printf("%f Fahrenheit is %.2f Celsius.\n", fahr, cels);
+    }
+}
