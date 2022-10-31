5daeef751072459037a65210c53d832effb10ce2
DesigneR- <limjunyi03@gmail.com>
2022-10-31 11:27:32 +0800

    Create L2Q2.java

---
+++ Lab 02/L2Q2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class L2Q2 {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        double P = sc.nextDouble(), 
+               D = sc.nextDouble(), 
+               R = sc.nextDouble(), 
+               Y = sc.nextDouble();
+
+        double M = (P - D) * (1 + R * Y / 100) / (Y * 12);
+
+        System.out.printf("The monthly payment is %.2f\n", M);
+    }
+}
