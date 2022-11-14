53b3acfe40910e66bdce341eef188161da1a0a71
Lim JY <limjunyi03@gmail.com>
2022-11-14 12:37:58 +0000

    Create Tester.java

---
+++ Lab 09/L9Q1/Tester.java
@@ -0,0 +1,15 @@
+package L9Q1;
+
+public class Tester {
+
+    public static void main(String[] args) {
+        
+        Rectangle rectangle = new Rectangle();
+        Square square = new Square();
+        Circle circle = new Circle();
+
+        rectangle.showDetails();
+        square.showDetails();
+        circle.showDetails();
+    }
+}
