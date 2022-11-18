3dee09bff70ecb21934193b2f35b58e48ca89397
Lim JY <limjunyi03@gmail.com>
2022-11-18 13:46:57 +0000

    Create Tester.java

---
+++ Lab 09/L9Q2/Tester.java
@@ -0,0 +1,10 @@
+package L9Q2;
+
+public class Tester {
+    
+    public static void main(String[] args) {
+        
+        Student s1 = new Student("Ali", 'M', "01-01-2022", "course.txt");
+        s1.displayDetails();
+    }
+}
