152aec24f75e33156650d974cc9d265a59113900
Lim JY <limjunyi03@gmail.com>
2022-11-18 13:46:07 +0000

    Create PersonProfile.java

---
+++ Lab 09/L9Q2/PersonProfile.java
@@ -0,0 +1,19 @@
+package L9Q2;
+
+public class PersonProfile {
+
+    protected final String NAME, DOB;
+    protected final char GENDER;
+
+    // Constructor
+    public PersonProfile(String name, char gender, String dob) {
+        this.NAME = name;
+        this.GENDER = gender;
+        this.DOB = dob;
+    }
+
+    // Method
+    public void displayProfile() {
+        System.out.printf("Name: %s\nGender: %s\nBirth: %s\n", this.NAME, this.GENDER, this.DOB);
+    }
+}
