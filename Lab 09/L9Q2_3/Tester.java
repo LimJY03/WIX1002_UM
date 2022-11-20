102e3abda480c68e4edcb60487bd90c70f60e0ef
DesigneR- <limjunyi03@gmail.com>
2022-11-20 20:11:32 +0800

    Update Tester.java

--- Lab 09/L9Q2_3/Tester.java
+++ Lab 09/L9Q2_3/Tester.java
@@ -3,8 +3,13 @@ package L9Q2_3;
 public class Tester {
     
     public static void main(String[] args) {
-        
+
+        // Test Constructor
         Student s1 = new Student("Ali", 'M', "01-01-2022", "course.txt");
+        Lecturer l1 = new Lecturer("Abu", 'F', "01-01-1990", "lecturer.txt");
+
+        // Test Methods
         s1.displayDetails();
+        l1.displayDetails();
     }
 }
