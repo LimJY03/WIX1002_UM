b2385bfbf6d96966c76298a21f1ca21a8ccaf559
DesigneR- <limjunyi03@gmail.com>
2022-11-22 19:38:20 +0800

    Create Tester.java

---
+++ Lab 10/L10Q4/Tester.java
@@ -0,0 +1,12 @@
+package L10Q4;
+
+/* Date Format = "ddMMyyyy:hh" */
+
+public class Tester {
+
+    public static void main(String[] args) {
+
+        Appointment apmt = new Appointment();
+        apmt.book("05122022:21", "11122022:20");
+    }
+}
