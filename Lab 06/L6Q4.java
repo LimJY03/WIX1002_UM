39eca6e0cbaa2e84acd6b0bcd0a41bac25e83bbf
Lim JY <limjunyi03@gmail.com>
2025-12-07 18:57:38 +0800

    refined logic

--- Lab 06/L6Q4.java
+++ Lab 06/L6Q4.java
@@ -10,21 +10,8 @@ public class L6Q4 {
 
     int getGCD(int q, int d) {
 
-        L6Q4 question = new L6Q4();
-        int temp = 0;
-
-        if (q < d) {
-            temp = q;
-            q = d;
-            d = temp;
-        }
-
-        temp = q % d;
-        q = d;
-        d = temp;
-
         if (d == 0) { return q; }
 
-        return question.getGCD(q, d);
+        return question.getGCD(d, q % d);
     }
 }
